28c2ba83fc0209a03b95a0ed9eb1bc4d75014705
pinarcankaya <pinarunalcan50@gmail.com>
2020-12-13 02:01:13 +0100

    US_05 12-12-2020

---
+++ src/test/java/gmibank/com/tests/DataBase_OdevCozumu_Pinar.java
@@ -0,0 +1,78 @@
+package gmibank.com.tests;
+
+import gmibank.com.utilities.DatabaseConnector;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+public class DataBase_OdevCozumu_Pinar {
+
+    String userFirstnameQuery;
+    String customerPhoneNoQuery;
+    String userAndCustomerQuery;
+
+    List<Map<String, String>> userlistMap;
+    List<Map<String, String>> customerlistMap;
+    List<Map<String, String>> userAndCustomerListMap;
+
+
+    //    Given kullanici firstname'i Ali olanlarin email datalarini getirir
+    //    Then Kullanici email'i bos olan kullanicinin olmadigini dogrular
+    @Test
+    public void query1() throws SQLException {
+        userFirstnameQuery = "SELECT email" +
+                " FROM jhi_user" +
+                " WHERE first_name='Ali' ";
+
+        userlistMap = DatabaseConnector.getQueryAsAListOfMaps(userFirstnameQuery);
+
+        System.out.println("listMap :" + userlistMap);
+
+        for (Map<String, String> w : userlistMap) {
+            Assert.assertFalse(w.get("email").equals(""));
+        }
+    }
+
+
+    // Given  kullanici id'si 30000 den buyuk olanlarin  phone number'larini goruntuler
+    //  kullanici phone number'larinin 10 haneden buyuk olmadigini dogrular
+    @Test
+    public void query2() throws SQLException {
+        customerPhoneNoQuery = "SELECT phone_number" +
+                " FROM tp_customer" +
+                " WHERE id> " + 30000;
+
+        customerlistMap = DatabaseConnector.getQueryAsAListOfMaps(customerPhoneNoQuery);
+        System.out.println("customerlistMap : " + customerlistMap);
+        ///******************
+        System.out.println("numaralar arasinda (-) olmadan 10 haneli: ");
+        for (Map<String, String> w : customerlistMap) {
+
+            if (w.get("phone_number").replace("-", "").length() <= 10) {
+                System.out.println(w.get("phone_number").replace("-", ""));
+            }
+            Assert.assertFalse((w.get("phone_number").length() <= 10));
+            ///number uzunlugu normalde 10 olmali ama (-) isareti de hane sayisi olarak sayilmis yani bu bir bug'dir
+        }
+    }
+
+    // kullanici activated'i true olmayan kisilerin isim soyisim ve mobile numberlerini goruntuler
+    //kullanici activated'i true olmayan kisilerin sayisinin 46 oldugunu dogrular
+    @Test
+    public void query3() throws SQLException {
+        userAndCustomerQuery = "select tp_customer.phone_number,jhi_user.first_name,jhi_user.last_name\n" +
+                "from jhi_user\n" +
+                "join tp_customer\n" +
+                "on jhi_user.id=tp_customer.user_id\n" +
+                "where not activated='true' ";
+
+        userAndCustomerListMap=DatabaseConnector.getQueryAsAListOfMaps(userAndCustomerQuery);
+        System.out.println(userAndCustomerListMap.size());
+        Assert.assertEquals(userAndCustomerListMap.size(),45);
+    }
+    //datalarin sayisi degisebilir yeni degisiklige uygun guncelleyiniz...
+}
+
---
+++ src/test/java/gmibank/com/tests/DataBase_OdevCozumu_Samet.java
@@ -0,0 +1,63 @@
+package gmibank.com.tests;
+
+import gmibank.com.utilities.ConfigurationReader;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import gmibank.com.utilities.DatabaseConnector;
+
+public class DataBase_OdevCozumu_Samet {
+
+
+//    Given kullanici GMI Bank database'ine baglanir
+//    Scenario: Kullanici user ve customer tablosundaki verileri dogrular
+//    Given kullanici activated'i true olmayan kisilerin isim soyisim ve mobile numberlerini goruntuler
+
+    String userQuery1 = "SELECT first_name, email FROM "
+            + ConfigurationReader.getProperty("usersTableName")
+            +" WHERE first_name = 'Ali' ";
+    String userQuery2 = "SELECT mobile_phone_number FROM "
+            + ConfigurationReader.getProperty("customerTableName")
+            +" WHERE id>30000;";
+
+    List<String> userEmail = new ArrayList<>();
+    List<String> userPhoneNumber = new ArrayList<>();
+    ResultSet resultSet;
+
+
+    //    Scenario: Kullanici user tablosundaki verileri dogrular
+    //    Given kullanici firstname'i Ali olanlarin email datalarini getirir
+    //    Then Kullanici email'i bos olan kullanicinin olmadigini dogrular
+    @Test
+    public void test1 () throws SQLException {
+        resultSet = DatabaseConnector.getResultSet(userQuery1);
+        while (resultSet.next()){
+            userEmail.add(resultSet.getString(2));
+        }
+        System.out.println(userEmail);
+        Assert.assertFalse(userEmail.contains(""));
+    }
+
+    //    Scenario: Kullanici customer tablosundaki verileri dogrular
+    //    Given  kullanici id'si 30000 den buyuk olanlarin  phone number'larini goruntuler
+    //    Then  kullanici phone number'larinin 10 haneden buyuk olmadigini dogrular
+    @Test
+    public void test2 () throws SQLException {
+        resultSet = DatabaseConnector.getResultSet(userQuery2);
+        while (resultSet.next()){
+            String data = resultSet.getString(1);
+            data = data.replace("-","");
+            data = data.replace("+","");
+            userPhoneNumber.add(data);
+        }
+        System.out.println(userPhoneNumber);
+        for(String w : userPhoneNumber){
+            Assert.assertEquals(w.length(),11);
+        }
+    }
+
+}
