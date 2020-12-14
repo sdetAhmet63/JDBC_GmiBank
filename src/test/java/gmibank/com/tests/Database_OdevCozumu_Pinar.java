afe7d81ea9e3cc4e9a945ac6b1bcf821bffed579
pinarcankaya <pinarunalcan50@gmail.com>
2020-12-14 23:49:39 +0100

    develop branch create

--- src/test/java/gmibank/com/tests/Database_OdevCozumu_Pinar.java
+++ src/test/java/gmibank/com/tests/Database_OdevCozumu_Pinar.java
@@ -1,15 +1,14 @@
 package gmibank.com.tests;
 
-
 import gmibank.com.utilities.DatabaseConnector;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.sql.SQLException;
-
 import java.util.List;
 import java.util.Map;
 
-public class Database_OdevCozumu_Pinar {
+public class DataBase_OdevCozumu_Pinar {
 
     String userFirstnameQuery;
     String customerPhoneNoQuery;
@@ -17,16 +16,13 @@ public class Database_OdevCozumu_Pinar {
 
     List<Map<String, String>> userlistMap;
     List<Map<String, String>> customerlistMap;
-    List<Map<String, String>>  userAndCustomerListMap;
+    List<Map<String, String>> userAndCustomerListMap;
 
-  //  @Given("kullanici GMI Bank database'ine baglanir")
-    public void kullanici_GMI_Bank_database_ine_baglanir() {
-        //baglanti "DatabaseConnector.getQueryAsAListOfMaps" methodu ile yapildi
 
-    }
-
-  //  @Given("kullanici firstname'i Ali olanlarin email datalarini getirir")
-    public void kullanici_firstname_i_Ali_olanlarin_email_datalarini_getirir() throws SQLException {
+    //    Given kullanici firstname'i Ali olanlarin email datalarini getirir
+    //    Then Kullanici email'i bos olan kullanicinin olmadigini dogrular
+    @Test
+    public void query1() throws SQLException {
         userFirstnameQuery = "SELECT email" +
                 " FROM jhi_user" +
                 " WHERE first_name='Ali' ";
@@ -35,37 +31,23 @@ public class Database_OdevCozumu_Pinar {
 
         System.out.println("listMap :" + userlistMap);
 
-
-    }
-
-   // @Then("Kullanici email'i bos olan kullanicinin olmadigini dogrular")
-    public void kullanici_email_i_bos_olan_kullanicinin_olmadigini_dogrular() {
-
         for (Map<String, String> w : userlistMap) {
             Assert.assertFalse(w.get("email").equals(""));
         }
     }
 
-   // @Given("kullanici id'si {int} den buyuk olanlarin  phone number'larini goruntuler")
-    public void kullanici_id_si_den_buyuk_olanlarin_phone_number_larini_goruntuler(Integer idNumber) throws SQLException {
+
+    // Given  kullanici id'si 30000 den buyuk olanlarin  phone number'larini goruntuler
+    //  kullanici phone number'larinin 10 haneden buyuk olmadigini dogrular
+    @Test
+    public void query2() throws SQLException {
         customerPhoneNoQuery = "SELECT phone_number" +
                 " FROM tp_customer" +
-                " WHERE id> " + idNumber;
+                " WHERE id> " + 30000;
 
         customerlistMap = DatabaseConnector.getQueryAsAListOfMaps(customerPhoneNoQuery);
         System.out.println("customerlistMap : " + customerlistMap);
-
-    }
-
-   // @Then("kullanici phone number'larinin {int} haneden buyuk olmadigini dogrular")
-    public void kullanici_phone_number_larinin_haneden_buyuk_olmadigini_dogrular(Integer phoneNumber) throws SQLException {
-        customerPhoneNoQuery = "SELECT phone_number" +
-                " FROM tp_customer" +
-                " WHERE id > " + phoneNumber;
-
-        customerlistMap = DatabaseConnector.getQueryAsAListOfMaps(customerPhoneNoQuery);
-        System.out.println(customerlistMap);
-
+        ///******************
         System.out.println("numaralar arasinda (-) olmadan 10 haneli: ");
         for (Map<String, String> w : customerlistMap) {
 
@@ -77,8 +59,10 @@ public class Database_OdevCozumu_Pinar {
         }
     }
 
-   // @Given("kullanici activated'i true olmayan kisilerin isim soyisim ve mobile numberlerini goruntuler")
-    public void kullanici_activated_i_true_olmayan_kisilerin_isim_soyisim_ve_mobile_numberlerini_goruntuler() throws SQLException {
+    // kullanici activated'i true olmayan kisilerin isim soyisim ve mobile numberlerini goruntuler
+    //kullanici activated'i true olmayan kisilerin sayisinin 46 oldugunu dogrular
+    @Test
+    public void query3() throws SQLException {
         userAndCustomerQuery = "select tp_customer.phone_number,jhi_user.first_name,jhi_user.last_name\n" +
                 "from jhi_user\n" +
                 "join tp_customer\n" +
@@ -86,9 +70,9 @@ public class Database_OdevCozumu_Pinar {
                 "where not activated='true' ";
 
         userAndCustomerListMap=DatabaseConnector.getQueryAsAListOfMaps(userAndCustomerQuery);
-        System.out.println(userAndCustomerListMap);
-
-
+        System.out.println(userAndCustomerListMap.size());
+        Assert.assertEquals(userAndCustomerListMap.size(),39);
     }
-
+    //datalarin sayisi degisebilir yeni degisiklige uygun guncelleyiniz...
 }
+
